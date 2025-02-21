1441673b56ee5bc6679bac6528c7f70458d67ddf
Ajay Rajput <166752814+Ajay6387@users.noreply.github.com>
2025-02-21 11:06:52 +0530

    config_setting.py

--- config_setting.py
+++ config_setting.py
@@ -2,7 +2,7 @@ import os
 # import psycopg2
 # # Use environment variables for sensitive data (Best Practice)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","YOUR_OPENAI_API_KEY")
-DATABASE_URL = "postgresql://postgres:Ajay%40Rajput%409555@db.rxsqnewbzxstvrmteurc.supabase.co:6543/postgres"
+DATABASE_URL = "postgresql://postgres:Ajay%40Rajput%409555@db.rxsqnewbzxstvrmteurc.supabase.co:5432/postgres"
 
 DB_CONFIG = {
     "user": "postgres",  
